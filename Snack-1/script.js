087b6952e5910dc71955bf1504b6a5f70f531cab
Francesca-Lisi <francescalisi2143@gmail.com>
2025-03-31 22:26:56 +0200

    feat: add Bonus to exercise 1 and fix errors

--- Snack-1/script.js
+++ Snack-1/script.js
@@ -2,12 +2,34 @@ const getPostTitle = id => {
     const promessa = new Promise((resolve, reject) => {
         fetch(`https://dummyjson.com/posts/${id}`)
             .then(res => res.json())
-            .then(oggetto => resolve(oggetto))
+            .then(post => resolve(post.title))
             .catch(reject)
     })
     return promessa;
 }
 
+
+
+const getPost = id => {
+    return new Promise((resolve, reject) => {
+        fetch(`https://dummyjson.com/posts/${id}`)
+            .then(res => res.json())
+            .then(post => {
+                fetch(` https://dummyjson.com/users/${post.userId}`)
+                    .then(res => res.json())
+                    .then(user => resolve({ ...post, user }))
+                    .catch(reject)
+            })
+            .catch(reject)
+    })
+
+}
+
 getPostTitle(1)
-    .then(oggetto => console.log(oggetto.title))
-    .catch(error => console.error(error))
+    .then(title => console.log('Titolo del post:', title))
+    .catch(error => console.error(error));
+
+
+getPost(1)
+    .then(post => console.log('Post selezionato:', post))
+    .catch(error => console.error(error)) 
